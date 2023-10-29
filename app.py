eb810078349eb5b40f96032663bff0c4360784a2
gammaraylab <gammarayelectron@gmail.com>
2023-10-29 20:00:38 +0530

    adding basic & educational details form

--- app.py
+++ app.py
@@ -4,7 +4,30 @@ from flask import Flask, render_template, redirect, url_for, request, session
 app = Flask(__name__)  
 app.secret_key = 'your_secret_key'  # Replace 'your_secret_key' with a strong, unique secret key
 
-user_creds={'admin':'admin'}
+user_creds={'admin':'admin'}        #users creds. currently stored in dict.
+
+######### method for basic details
+@app.route('/basic_details',methods=['GET','POST'])
+def basicd_etails():
+    error=None
+    if request.method == 'POST':
+        name=request.form['name']
+        roll=request.form['roll']
+        department=request.form['department']
+        program=request.form['program']
+        year=request.form['year']
+
+        print(name,roll,department,program,year)
+    return 'nicly done basicd_etails'
+
+
+######### method for educational details
+@app.route('/edu_details',methods=['GET','POST'])
+def edu_details():
+    return 'nicly done edu_details'
+
+
+######### method to handle secure logins
 @app.route('/login',methods=['GET','POST']) 
 def login():
     error=None
@@ -18,6 +41,8 @@ def login():
             return render_template('filldetail.html',error="none")
     return render_template('login.html',error=error)
 
+
+######### method to register new users
 @app.route('/register',methods=['GET','POST']) 
 def register():
     error=None
@@ -33,16 +58,21 @@ def register():
             return render_template('login.html',error=None)
     return render_template('register.html',error=error)
 
+
+######### method to logout the current user
 @app.route('/logout')  
 def logout():  
     # Clear the session to log the user out
     print(session.pop('username', None),"has logged out!")
     return render_template('index.html')  
 
+
+######### root method 
 @app.route('/')  
 def index():  
     return render_template('index.html')  
 
+######### calling main method
 if __name__ == '__main__':  
     app.run(debug=True)  
 
