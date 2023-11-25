57b52c68eec470a5c93a3c9b447490b74d5ef4dd
gammaraylab <gammarayelectron@gmail.com>
2023-11-26 02:53:20 +0530

    moved all the DB operations and other utility to seperate py code

--- app.py
+++ app.py
@@ -1,90 +1,63 @@
 #!/usr/bin/python3
 from flask import Flask, render_template, redirect, url_for, request, session
 import psycopg2
+import json
 from user_management import User
 
 app = Flask(__name__)  
 app.secret_key = 'your_secret_key'  # Replace 'your_secret_key' with a strong, unique secret key
 
-# Database Configuration
-DB_NAME = "profiler_users_db"
-DB_USER = "profiler"
-DB_PASSWORD = "profiler"
-DB_HOST = "localhost"
-
-def connect_to_db():
-    return psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-
-#user_creds={'admin':'admin'}        #users creds. currently stored in dict.
-
-######### method for basic details
-@app.route('/basic_details',methods=['GET','POST'])
-def basic_details():
+######### route for getting details
+@app.route('/details',methods=['GET','POST'])
+def details():
     error=None
     if request.method == 'POST':
-        name=request.form['name']
-        roll=request.form['roll']
-        department=request.form['department']
-        program=request.form['program']
-        gender=request.form['gender']
-        user = User(name, roll, department, program, gender, null, null, null)
-        print(user)
-    return 'nicely done basic_details'
+        # get all details from the html form as JSON object and convert it Dict
+        completeUserData=request.json['completeData']   
+        # get the current user_id 
+        user_id=session['user_id']
+        #insert into db
+        insert_userdata_in_db(user_id,completeUserData)
+
+        return error="success"
+
+    return render_template('details.html',error="none")
 
 
-######### method for educational details
-@app.route('/edu_details',methods=['GET','POST'])
-def edu_details():
-    return 'nicely done edu_details'
-
-
-######### method to handle secure logins
+######### route to handle secure logins
 @app.route('/login',methods=['GET','POST']) 
 def login():
     error=None
     if request.method == 'POST':
-        uname=request.form['username']
-        upassword=request.form['password']
+        username=request.form['username']
+        password=request.form['password']
 
-        conn = connect_to_db()  #connecting to DB
-        cur = conn.cursor()
-        cur.execute(f"SELECT id FROM users WHERE username = '{uname}' AND password = '{upassword}'")
-        user_id = cur.fetchone()   #fetching the query results
-        conn.close() #closing the connection
+        # validating user's credentials
+        user_id=validate_user(username,password)
 
-        if  not user_id:
+        if user_id == -1:
             error='Invalid Credentials'
         else:
-            # Store the user's username in the session
-            session['username'] = uname
-            return render_template('basic_details.html',error="none")
+            # Store the user_id in the session
+            session['user_id'] = user_id
+            return render_template('details.html',error="none")
+
     return render_template('login.html',error=error)
 
-
-######### method to register new users
+######### route to register new users
 @app.route('/register',methods=['GET','POST']) 
 def register():
     error=None
     if request.method == 'POST':
-        conn=connect_to_db()
-        cur = conn.cursor()
         #username and password from the form
-        uname=request.form['username']    
-        upass=request.form['password']
+        username=request.form['username']    
+        password=request.form['password']
 
-        cur.execute(f"SELECT id FROM users WHERE username = '{uname}'")
-        user_exist = cur.fetchone()   
-        #checking if the entered username is available or not
-        if(user_exist): 
-            error='username taken'
-        elif (upass != request.form['retypepassword']):
+        if (password != request.form['retypepassword']):
             error='password didn\'t match'
-        #add the username and password into the users table
+        elif not registration(username,password):
+            error = "registration failed"
         else:
-            cur.execute("INSERT INTO users(username,password) VALUES( %s, %s)", (uname,upass)) #executing query
-            conn.commit()
-            cur.close()
-            conn.close()
             return render_template('login.html',error="registration successful.")
 
     return render_template('register.html',error=error)
@@ -93,16 +66,18 @@ def register():
 ######### method to logout the current user
 @app.route('/logout')  
 def logout():  
-    # Clear the session to log the user out
-    print(session.pop('username', None),"has logged out!")
-    return render_template('index.html')  
+    if request.method == 'POST':
+        # Clear the session to log the user out
+        print(session.pop('user_id', None),"has logged out!")
+        return render_template('index.html')  
+
 
 ######### Route to download the generated PDF resume
 @app.route('/download_resume', methods=['GET'])
 def download_resume():
     if 'user' in session:
         #get user data from database
-        user = next((user for user in users if user.email == session['user']), None)
+        # user get the users data from the db
         mapping_path = 'data_mapping.json'
         template_path = 'template/SWL_RESUME_TEMPLATE.tex'
 
